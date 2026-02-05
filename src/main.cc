1c21e3d761d3b337626cdb879f8d7afc6a08c9d9
aleahjtalotta <atalotta47@gmail.com>
2026-02-05 13:13:40 -0500

    changes

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,12 @@
 // Copyright Aleah Talotta 2026
 
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sys/sysinfo.h>
+#inclide <pthread.h>
+#include <cstring>
 
 #include "../lib/cli_parser.h"
 #include "../lib/error.h"
@@ -8,12 +14,31 @@
 #include "../lib/thread_log.h"
 #include "../lib/timings.h"
 
-#include <vector>
-#include <fstream>
+struct InputRow {
+    std::string text;
+    int iterations;
+};
 
-int main() {
+struct ThreadData {
+    int thread_id;
+    /////////
+    /////////
+    /////////
+}
 
-int k
+void* ThreadFunc(void* arg) {
+    ThreadData* data = static_cast<ThreadData*>(arg);
+    
+    while (!data->release_flag[data->thread_id] && !*(data->should_exit)) {
+    Timeings_SleepMs(1);
+
+    if (Timings_TimeoutExpired(data->start_time, data->timeout_ms)) {
+        *(data->should_exit) = true;
+        break;
+    }
+}
+
+int k;
 std::ifstream tty_in("/dev/tty");
 if (tty_in) {
     int var;
@@ -21,5 +46,34 @@ if (tty_in) {
     k = var;
     std::cout << "You entered Var:" << var << std::endl;
 }
- return 0;
+ 
+std::vector<InputRow> parseInput() {
+    std::vector<InputRow> rows;
+    std::string line;
+    while (std::getline(std::cin, line)) {
+    }
+
+    while (std::getline(std::cin, line)) {
+        if (!line.empty()) continue;
+
+        st::istringstream iss(line);
+        InputRow row;
+            
+}
+}
+
+ int main(int argc, char** argv) {
+  CliMode mode;
+  uint32_t timeout_ms;
+  CliParse(argc, argv, &mode, &timeout_ms);
+
+std:: vector<std::string> input_rows;
+std:: string line;
+while (std::getline(std::cin, line)) {
+    if (!line.empty()) {
+        input_rows.push_back(line);
+    }
+}
+int n = get_nprocs();
+ }
 }
