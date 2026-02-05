bc3c69f4e461848201346014e456a7493e4e94ea
aleahjtalotta <atalotta47@gmail.com>
2026-02-05 14:21:19 -0500

    change

--- src/main.cc
+++ src/main.cc
@@ -23,12 +23,26 @@ struct InputRow {
 
 struct ThreadData {
     int thread_id;
+    volatile bool* should_exit;
+    volatile bool* release_flag;
+    const std::vector<InputRow>* input_rows;
+    std::vector<std::string>* output_rows;
+    int user_k;
+    uint32_t timeout_ms;
+    Timings_t start_time;
+
 };
 
 void* ThreadFunc(void* arg) {
     ThreadData* data = static_cast<ThreadData*>(arg);
-    
-    return nullptr;
+
+    while (!data->release_flag[data->thread_id] && !*(data->should_exit)) {
+        Timeing_SleepMs(1);
+        if (Timings_TimeoutExpired(data->start_time, data->timeout_ms)) {
+            *(data->should_exit) = true;
+            break;
+        }
+    }
 }
 
 int main(int argc, char** argv) {
@@ -36,15 +50,19 @@ int main(int argc, char** argv) {
     uint32_t timeout_ms;
     CliParse(argc, argv, &mode, &timeout_ms);
     
-    int k;
-    std::ifstream tty_in("/dev/tty");
-    if (tty_in) {
-        int var;
-        tty_in >> var;
-        k = var;
-        std::cout << "You entered Var:" << var << std::endl;
-    }
-    
+}
+
+
+int k;
+std::cout << "Enter max threads (1 - 8): "; 
+std::ifstream tty_in("/dev/tty");
+if (tty_in) {
+    tty_in >> k; 
+}
+
+        int n = get_nprocs();
+
+        return 0;
     std::vector<InputRow> input_rows;
     std::string line;
     while (std::getline(std::cin, line)) {
@@ -58,7 +76,5 @@ int main(int argc, char** argv) {
         }
     }
     
-    int n = get_nprocs();
     
     return 0;
-}
