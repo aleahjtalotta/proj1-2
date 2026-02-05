418d825fae945889d8e036598c43e281e9b5e85b
aleahjtalotta <atalotta47@gmail.com>
2026-02-05 11:08:57 -0500

    change

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,11 @@
 // Copyright Aleah Talotta 2026
 
 #include <iostream>
-#include <cli_parser.h>
-#include <error.h>
-#include <thread_log.h>
-#include <timings.h>
-#include <sha256.h>
+#include "cli_parser.h"
+#include "error.h"
+#include "thread_log.h"
+#include "timings.h"
+#include "sha256.h"
 
 #include <vector>
 #include <fstream>
